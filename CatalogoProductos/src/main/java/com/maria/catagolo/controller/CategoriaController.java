42fc87994e7babce797948d03a5c15e7a316ccaf
mariagm173 <mariagm173@hotmail.com>
2020-04-02 23:51:34 +0200

    CatalogoProductos 001 - Proyecto completo con Thymeleaf

---
+++ CatalogoProductos/src/main/java/com/maria/catagolo/controller/CategoriaController.java
@@ -0,0 +1,79 @@
+package com.maria.catagolo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.maria.catagolo.model.Categoria;
+import com.maria.catagolo.service.CategoriaService;
+import com.maria.catagolo.service.ProductoService;
+
+@Controller
+@RequestMapping("/admin/categoria")
+public class CategoriaController {
+
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	@Autowired
+	private ProductoService productoService;
+	
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("categorias", categoriaService.findAll());
+		return "admin/list-categoria";
+	}
+	
+	@GetMapping("/nueva")
+	public String nuevaCategoria(Model model) {
+		model.addAttribute("categoria", new Categoria());
+		return "admin/form-categoria";
+	}
+	
+	@PostMapping("/nueva/submit")
+	public String submitNuevaCategoria(@ModelAttribute("categoria") Categoria categoria, Model model) {
+		
+		categoriaService.save(categoria);
+		
+		return "redirect:/admin/categoria/";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String editarCategoria(@PathVariable("id") Long id, Model model) {
+		
+		Categoria categoria = categoriaService.findById(id);
+		
+		if (categoria != null) {
+			model.addAttribute("categoria", categoria);
+			return "admin/form-categoria";
+		} else {
+			return "redirect:/admin/categoria/";
+		}
+		
+	}
+	
+	@GetMapping("/borrar/{id}")
+	public String borrarCategoria(@PathVariable("id") Long id, Model model) {
+		
+		Categoria categoria = categoriaService.findById(id);
+		
+		if (categoria != null) {
+			
+			if (productoService.numeroProductosCategoria(categoria) == 0) {
+				categoriaService.delete(categoria);				
+			} else {
+				return "redirect:/admin/categoria/?error=true";
+			}
+			
+		} 
+
+		return "redirect:/admin/categoria/";
+		
+		
+	}
+}
---
+++ CatalogoProductos/src/main/java/com/maria/catagolo/repository/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package com.maria.catagolo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.maria.catagolo.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	
+	@Query("select c from Categoria c where c.destacada = true")
+	public List<Categoria> findDestacadas();
+}
